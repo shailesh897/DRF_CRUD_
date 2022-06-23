9d3c1947809032af3e33ac8b8fac4fbdb800eb96
shailesh897 <shailesh.p897@gmail.com>
2022-06-23 13:27:04 +0530

    Created migrations , added view function with get and added view function add_items

--- DRF/api/views.py
+++ DRF/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from rest_framework import serializers
+from rest_framework import status
 from .models import Item
 from .serializers import ItemSerializer
 
@@ -17,3 +18,18 @@ def ApiOverview(request):
     }
 
     return Response(api_urls)
+
+@api_view(['POST'])
+def add_items(request):
+	item = ItemSerializer(data=request.data)
+
+	# validating for already existing data
+	if Item.objects.filter(**request.data).exists():
+		raise serializers.ValidationError('This data already exists')
+
+	if item.is_valid():
+		item.save()
+		return Response(item.data)
+	else:
+		return Response(status=status.HTTP_404_NOT_FOUND)
+
