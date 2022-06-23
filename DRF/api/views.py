d76c1181aca7082362158663c8fdcafc7018b6f5
shailesh897 <shailesh.p897@gmail.com>
2022-06-23 13:33:26 +0530

    added update_items view function

--- DRF/api/views.py
+++ DRF/api/views.py
@@ -19,34 +19,43 @@ def ApiOverview(request):
 
     return Response(api_urls)
 
-@api_view(['POST'])
+
+@api_view(["POST"])
 def add_items(request):
-	item = ItemSerializer(data=request.data)
-	# if already existing data
-	if Item.objects.filter(**request.data).exists():
-		raise serializers.ValidationError('This data already exists')
+    item = ItemSerializer(data=request.data)
+    # if already existing data
+    if Item.objects.filter(**request.data).exists():
+        raise serializers.ValidationError("This data already exists")
 
-	if item.is_valid():
-		item.save()
-		return Response(item.data)
-	else:
-		return Response(status=status.HTTP_404_NOT_FOUND)
+    if item.is_valid():
+        item.save()
+        return Response(item.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def view_items(request):
-	# checking for the parameters from the URL
-	if request.query_params:
-		items = Item.objects.filter(**request.query_param.dict())
-	else:
-		items = Item.objects.all()
+    # checking for the parameters from the URL
+    if request.query_params:
+        items = Item.objects.filter(**request.query_param.dict())
+    else:
+        items = Item.objects.all()
 
-	# if there is something in items else raise error
-	if items:
-		data = ItemSerializer(items)
-		return Response(data)
-	else:
-		return Response(status=status.HTTP_404_NOT_FOUND)
+    # if there is something in items else raise error
+    if items:
+        data = ItemSerializer(items)
+        return Response(data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
 
-    
+@api_view(["POST"])
+def update_items(request, pk):
+    item = Item.objects.get(pk=pk)
+    data = ItemSerializer(instance=item, data=request.data)
+    if data.is_valid():
+        data.save()
+        return Response(data.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
